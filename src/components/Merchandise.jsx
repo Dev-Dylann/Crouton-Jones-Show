038f23fe73c7c517d15f7f67f321d793cfef6120
Tubi Tunmise <tubitunmise@gmail.com>
2024-11-11 16:06:03 +0100

    styled merch for tablet screen

--- src/components/Merchandise.jsx
+++ src/components/Merchandise.jsx
@@ -4,17 +4,17 @@ import MerchCard from "../ui/MerchCard"
 
 const Merchandise = () => {
     return (
-        <section className="py-16 px-5 gap-4 flex flex-col">
+        <section className="py-16 px-5 gap-4 flex flex-col md:py-28 md:px-8 md:gap-8">
             <motion.h2
                 initial={{ opacity: 0, y: 25 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ type: 'tween', duration: 0.5 }}
                 viewport={{ once: true }}
-                className="font-impact font-bold text-primaryBtn text-2xl text-center tracking-widest heading uppercase"
+                className="font-impact font-bold text-primaryBtn text-2xl text-center tracking-widest heading uppercase md:text-5xl"
             >Explore Our Merch</motion.h2>
 
             <article
-                className="flex flex-col gap-10 p-5"
+                className="flex flex-col gap-10 p-5 md:gap-12 md:p-8 md:grid md:grid-cols-2"
             >
                 {data.merchandise.map((merch, index) => (
                     <MerchCard key={`merch${index}`} merch={merch} />
--- src/ui/MerchCard.jsx
+++ src/ui/MerchCard.jsx
@@ -8,23 +8,23 @@ const MerchCard = ({ merch }) => {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ type: 'tween', duration: 0.5 }}
             viewport={{ once: true }}
-            className="flex flex-col bg-white overflow-hidden rounded-lg shadow-lg shadow-textColor/50">
+            className="flex flex-col bg-white overflow-hidden rounded-lg shadow-lg shadow-textColor/50 md:h-fit">
             <img src={merch.image} alt={merch.name} className="rounded-lg" />
 
-            <div className="px-5 py-2 flex flex-col gap-1">
-                <p className="font-bold">{merch.name}</p>
+            <div className="px-5 py-2 flex flex-col gap-1 md:py-5 md:gap-3">
+                <p className="font-bold md:text-xl">{merch.name}</p>
 
                 <div className="flex gap-8 py-0.5">
-                    <p className='flex text-sm items-center gap-1'>
-                        <CurrencyDollarIcon className="h-5 w-5" />
+                    <p className='flex text-sm items-center gap-1 md:text-base md:gap-2'>
+                        <CurrencyDollarIcon className="h-5 w-5 md:h-8 md:w-8" />
                         USD
                         <span className="font-bold">{(merch.price.usd / 100).toFixed(2)}</span>
                     </p>
 
-                    <p className='flex text-sm items-center gap-1'>
-                        <CircleStackIcon className="h-5 w-5" />
+                    <p className='flex text-sm items-center gap-1 md:text-base md:gap-2'>
+                        <CircleStackIcon className="h-5 w-5 md:h-8 md:w-8" />
                         CJT
-                        <span className="font-bold">{merch.price.cjt}</span>
+                        <span className="font-bold">{merch.price.cjt || "N/A"}</span>
                     </p>
                 </div>
             </div>
