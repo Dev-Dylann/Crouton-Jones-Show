213a558ec50fabee266172ed685979746d61e718
Tubi Tunmise <tubitunmise@gmail.com>
2024-11-11 15:34:50 +0100

    stled intro for tablet screen

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,14 +2,14 @@ import { motion } from "framer-motion"
 
 const Intro = () => {
     return (
-        <section className="py-16 px-5 flex flex-col items-center gap-10" >
-            <article className='flex flex-col gap-4'>
+        <section className="py-16 px-5 flex flex-col items-center gap-10 md:py-28 md:px-8" >
+            <article className='flex flex-col gap-4 md:gap-8'>
                 <motion.h2
                     initial={{ opacity: 0, y: 25 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="font-impact font-bold text-primaryBtn text-2xl text-center tracking-widest heading"
+                    className="font-impact font-bold text-primaryBtn text-2xl text-center tracking-widest heading md:text-5xl"
                 >CROUTON JONES: The Coolest Detective In New Snack City</motion.h2>
 
                 <motion.p
@@ -17,7 +17,7 @@ const Intro = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="font-semibold text-center"
+                    className="font-semibold text-center md:text-xl"
                 >Get ready to dive into the smoothest, most action-packed adventure in New Snack City! Watch Crouton Jones, the fearless detective with style, wit, and his iconic catchphrase, "Respect it, baby!" as he battles the city's most notorious crime families. This is just a taste of what's to come in the Croutonverse. Stay tuned, baby!</motion.p>
 
                 <motion.video
@@ -26,19 +26,19 @@ const Intro = () => {
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
                     controls
-                    className="py-4"
+                    className="py-4 md:py-8 md:w-[85%] md:mx-auto"
                 >
                     <source src="./assets/teaser.mp4" type="video/mp4" />
                 </motion.video>
             </article>
 
-            <article className="flex flex-col gap-4">
+            <article className="flex flex-col gap-4 md:gap-8">
                 <motion.h2
                     initial={{ opacity: 0, y: 25 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="font-impact font-bold text-primaryBtn text-2xl text-center tracking-widest heading uppercase"
+                    className="font-impact font-bold text-primaryBtn text-2xl text-center tracking-widest heading uppercase md:text-5xl"
                 >About Us</motion.h2>
 
                 <motion.p
@@ -46,7 +46,7 @@ const Intro = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="font-semibold text-center"
+                    className="font-semibold text-center md:text-xl"
                 >Crouton Jones isn&apos;t just a character, he&apos;s the meme king and the heart of a first-of-its-kind Global Brand Token. Powered by the $CJT token, this project is creating a whole new way to engage with a brand, allowing fans to own a piece of the Crouton Jones universe.
 
                     If you want to be part of a revolutionary movement that blends entertainment, blockchain, and community-driven content, you&apos;ve come to the right place!</motion.p>
@@ -56,7 +56,7 @@ const Intro = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className='uppercase font-impact tracking-widest text-lg px-6 py-3 bg-primaryBtn rounded-lg shadow-lg shadow-textColor/50 font-bold w-fit self-center'
+                    className='uppercase font-impact tracking-widest text-lg px-6 py-3 bg-primaryBtn rounded-lg shadow-lg shadow-textColor/50 font-bold w-fit self-center md:text-2xl'
                 >Read More</motion.button>
             </article>
         </section>
