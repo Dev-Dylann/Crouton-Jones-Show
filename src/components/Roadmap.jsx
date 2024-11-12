1e480b23c84ff543f4ae198364b341a95d7d2508
Tubi Tunmise <tubitunmise@gmail.com>
2024-11-12 20:05:54 +0100

    styled roadmap for tablet screen

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -11,7 +11,7 @@ const Roadmap = () => {
                 className="font-impact font-bold text-primaryBtn text-2xl text-center tracking-widest heading uppercase md:text-[40px]"
             >The Crouton Jones Journey</motion.h2>
 
-            <article className="flex flex-col gap-28 relative my-5">
+            <article className="flex flex-col gap-28 relative my-5 md:gap-36">
                 <div className="absolute h-full w-1.5 bg-white z-[1] inset-0 mx-auto"></div>
 
                 <motion.div
@@ -19,10 +19,10 @@ const Roadmap = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="bg-white shadow-lg flex flex-col gap-2 shadow-textColor/50 rounded-lg p-5 z-[2]">
-                    <h3 className="font-impact text-xl tracking-wider font-bold uppercase text-primaryBtn">Meet Crouton Jones</h3>
+                    className="bg-white shadow-lg flex flex-col gap-2 shadow-textColor/50 rounded-lg p-5 z-[2] md:p-8 md:gap-4">
+                    <h3 className="font-impact text-xl tracking-wider font-bold uppercase text-primaryBtn md:text-[28px]">Meet Crouton Jones</h3>
 
-                    <p className='font-semibold'>Dive into the world of Crouton Jones, the smooth-talking, afro-wearing hero with a flair for adventure.</p>
+                    <p className='font-semibold md:text-xl'>Dive into the world of Crouton Jones, the smooth-talking, afro-wearing hero with a flair for adventure.</p>
                 </motion.div>
 
                 <motion.div
@@ -30,10 +30,10 @@ const Roadmap = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="bg-white shadow-lg flex flex-col gap-2 shadow-textColor/50 rounded-lg p-5 z-[2]">
-                    <h3 className="font-impact text-xl tracking-wider font-bold uppercase text-primaryBtn">Crouton Jones Comic Series</h3>
+                    className="bg-white shadow-lg flex flex-col gap-2 shadow-textColor/50 rounded-lg p-5 z-[2] md:p-8 md:gap-4">
+                    <h3 className="font-impact text-xl tracking-wider font-bold uppercase text-primaryBtn md:text-[28px]">Crouton Jones Comic Series</h3>
 
-                    <p className='font-semibold'>Step into the illustrated universe of Crouton Jones through his exclusive comic series.</p>
+                    <p className='font-semibold md:text-xl'>Step into the illustrated universe of Crouton Jones through his exclusive comic series.</p>
                 </motion.div>
 
                 <motion.div
@@ -41,10 +41,10 @@ const Roadmap = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="bg-white shadow-lg flex flex-col gap-2 shadow-textColor/50 rounded-lg p-5 z-[2]">
-                    <h3 className="font-impact text-xl tracking-wider font-bold uppercase text-primaryBtn">CJ Meme Cartoons</h3>
+                    className="bg-white shadow-lg flex flex-col gap-2 shadow-textColor/50 rounded-lg p-5 z-[2] md:p-8 md:gap-4">
+                    <h3 className="font-impact text-xl tracking-wider font-bold uppercase text-primaryBtn md:text-[28px]">CJ Meme Cartoons</h3>
 
-                    <p className='font-semibold'>Experience the lighter side of Crouton Jones with our Meme Cartoon Series.</p>
+                    <p className='font-semibold md:text-xl'>Experience the lighter side of Crouton Jones with our Meme Cartoon Series.</p>
                 </motion.div>
 
                 <motion.div
@@ -52,10 +52,10 @@ const Roadmap = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="bg-white shadow-lg flex flex-col gap-2 shadow-textColor/50 rounded-lg p-5 z-[2]">
-                    <h3 className="font-impact text-xl tracking-wider font-bold uppercase text-primaryBtn">Launch of CJ NFT Coin</h3>
+                    className="bg-white shadow-lg flex flex-col gap-2 shadow-textColor/50 rounded-lg p-5 z-[2] md:p-8 md:gap-4">
+                    <h3 className="font-impact text-xl tracking-wider font-bold uppercase text-primaryBtn md:text-[28px]">Launch of CJ NFT Coin</h3>
 
-                    <p className='font-semibold'>Join us as we bring Crouton Jones into the future with the launch of the CJ NFT Coin</p>
+                    <p className='font-semibold md:text-xl'>Join us as we bring Crouton Jones into the future with the launch of the CJ NFT Coin</p>
                 </motion.div>
 
                 <motion.button
@@ -63,7 +63,7 @@ const Roadmap = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className='uppercase font-impact tracking-widest text-lg px-6 py-3 bg-primaryBtn rounded-lg shadow-lg shadow-textColor/50 font-bold w-fit self-center z-[2]'
+                    className='uppercase font-impact tracking-widest text-lg px-6 py-3 bg-primaryBtn rounded-lg shadow-lg shadow-textColor/50 font-bold w-fit self-center z-[2] md:text-2xl'
                 >See Full Roadmap</motion.button>
             </article>
         </section>
