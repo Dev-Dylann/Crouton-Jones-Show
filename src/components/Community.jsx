7b357623c2c1c83d18686529cd0b0fdc75e4e959
Tubi Tunmise <tubitunmise@gmail.com>
2024-11-13 15:47:45 +0100

    finished styling for lg

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -5,13 +5,13 @@ import jonesLeather from '../assets/images/jones-leather.webp'
 
 const Community = () => {
     return (
-        <section className="px-5 pt-16 pb-60 flex flex-col gap-4 bg-primaryBtn bg-[url('./background/scratches.webp')] bg-cover bg-center relative md:px-8 sm:py-16">
+        <section className="px-5 pt-16 pb-60 flex flex-col gap-4 bg-primaryBtn bg-[url('./background/scratches.webp')] bg-cover bg-center relative md:px-8 sm:py-16 lg:px-12 lg:grid lg:grid-cols-2">
             <motion.h2
                 initial={{ opacity: 0, y: 25 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ type: 'tween', duration: 0.5 }}
                 viewport={{ once: true }}
-                className="font-impact font-bold text-white text-2xl text-center tracking-widest heading uppercase"
+                className="font-impact font-bold text-white text-2xl text-center tracking-widest heading uppercase lg:text-4xl lg:col-span-full"
             >Join Our Community</motion.h2>
 
             <motion.p
@@ -19,10 +19,10 @@ const Community = () => {
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ type: 'tween', duration: 0.5 }}
                 viewport={{ once: true }}
-                className="font-semibold text-center"
+                className="font-semibold text-center lg:col-span-full lg:text-xl"
             >Connect with fans, speak to the creators, get exclusive updates, and be part of the Crouton Jones adventure.</motion.p>
 
-            <div className="flex flex-col items-center gap-6 my-4 sm:items-end sm:px-20">
+            <div className="flex flex-col items-center gap-6 my-4 sm:items-end sm:px-20 lg:order-2 lg:place-content-center lg:items-center">
                 <motion.button
                     initial={{ opacity: 0, y: 25 }}
                     whileInView={{ opacity: 1, y: 0 }}
@@ -53,7 +53,7 @@ const Community = () => {
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ type: 'tween', duration: 0.5 }}
                 viewport={{ once: true }}
-                className="absolute bottom-0 w-1/2 mx-auto max-w-[250px]"
+                className="absolute bottom-0 w-1/2 mx-auto max-w-[250px] lg:relative lg:order-2 lg:w-full lg:max-w-[300px]"
             />
         </section>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,24 +9,24 @@ import { motion } from "framer-motion"
 
 const Footer = () => {
     return (
-        <section className="py-16 px-5 flex flex-col items-center gap-10 md:px-8 md:py-24">
+        <section className="py-16 px-5 flex flex-col items-center gap-10 md:px-8 md:py-24 lg:px-12 lg:grid lg:grid-cols-3 lg:gap-x-0 lg:gap-y-16">
             <motion.img
                 src={logo}
                 alt="Crouton Jones Logo"
-                className="w-[80%]"
+                className="w-[80%] lg:self-start lg:col-span-1 lg:w-full"
                 initial={{ opacity: 0, y: 25 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ type: 'tween', duration: 0.5 }}
                 viewport={{ once: true }}
             />
 
-            <nav className="flex flex-col gap-4 font-impact tracking-wider text-lg text-center uppercase md:text-xl">
+            <nav className="flex flex-col gap-4 font-impact tracking-wider text-lg text-center uppercase md:text-xl lg:flex-row lg:text-left lg:col-span-2 lg:pt-8 lg:gap-20 lg:row-span-2 lg:place-self-start lg:justify-end lg:w-full">
                 <motion.ul
                     initial={{ opacity: 0, y: 25 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="flex flex-col gap-1"
+                    className="flex flex-col gap-1 lg:gap-4"
                 >
                     <li>Roadmap</li>
                     <li>Characters</li>
@@ -38,7 +38,7 @@ const Footer = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="flex flex-col gap-1"
+                    className="flex flex-col gap-1 lg:gap-4"
                 >
                     <li>Comic</li>
                     <li>Merch</li>
@@ -50,7 +50,7 @@ const Footer = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="flex flex-col gap-1"
+                    className="flex flex-col gap-1 lg:gap-4"
                 >
                     <li>Contact Us</li>
                     <li>Privacy Policy</li>
@@ -58,25 +58,25 @@ const Footer = () => {
                 </motion.ul>
             </nav>
 
-            <hr className="border-textColor w-full" />
+            <hr className="border-textColor w-full lg:hidden" />
 
-            <article className=" flex flex-col gap-2">
+            <article className="flex flex-col gap-2">
                 <motion.p
                     initial={{ opacity: 0, y: 25 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ type: 'tween', duration: 0.5 }}
                     viewport={{ once: true }}
-                    className="uppercase font-impact text-lg text-center tracking-widest font-bold"
+                    className="uppercase font-impact text-lg text-center tracking-widest font-bold lg:text-sm lg:text-left"
                 >Follow Our Social Media</motion.p>
 
-                <div className="flex items-center justify-center flex-wrap gap-2">
+                <div className="flex items-center justify-center flex-wrap gap-2 lg:justify-start">
                     <motion.button
                         initial={{ opacity: 0, y: 25 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         transition={{ type: 'spring', duration: 0.5 }}
                         viewport={{ once: true }}
                         className="rounded-lg shadow-lg shadow-textColor/50 border border-textColor p-2">
-                        <img src={discord} alt="Discord" className="w-8 h-8" />
+                        <img src={discord} alt="Discord" className="w-6 h-6" />
                     </motion.button>
                     <motion.button
                         initial={{ opacity: 0, y: 25 }}
@@ -84,7 +84,7 @@ const Footer = () => {
                         transition={{ type: 'spring', duration: 0.5, delay: 0.1 }}
                         viewport={{ once: true }}
                         className="rounded-lg shadow-lg shadow-textColor/50 border border-textColor p-2">
-                        <img src={telegram} alt="Telegram" className="w-8 h-8" />
+                        <img src={telegram} alt="Telegram" className="w-6 h-6" />
                     </motion.button>
                     <motion.button
                         initial={{ opacity: 0, y: 25 }}
@@ -92,7 +92,7 @@ const Footer = () => {
                         transition={{ type: 'spring', duration: 0.5, delay: 0.2 }}
                         viewport={{ once: true }}
                         className="rounded-lg shadow-lg shadow-textColor/50 border border-textColor p-2">
-                        <img src={twitter} alt="Twitter/X" className="w-8 h-8" />
+                        <img src={twitter} alt="Twitter/X" className="w-6 h-6" />
                     </motion.button>
                     <motion.button
                         initial={{ opacity: 0, y: 25 }}
@@ -100,7 +100,7 @@ const Footer = () => {
                         transition={{ type: 'spring', duration: 0.5, delay: 0.3 }}
                         viewport={{ once: true }}
                         className="rounded-lg shadow-lg shadow-textColor/50 border border-textColor p-2">
-                        <img src={youtube} alt="Youtube" className="w-8 h-8" />
+                        <img src={youtube} alt="Youtube" className="w-6 h-6" />
                     </motion.button>
                     <motion.button
                         initial={{ opacity: 0, y: 25 }}
@@ -108,7 +108,7 @@ const Footer = () => {
                         transition={{ type: 'spring', duration: 0.5, delay: 0.4 }}
                         viewport={{ once: true }}
                         className="rounded-lg shadow-lg shadow-textColor/50 border border-textColor p-2">
-                        <img src={instagram} alt="Instagram" className="w-8 h-8" />
+                        <img src={instagram} alt="Instagram" className="w-6 h-6" />
                     </motion.button>
                     <motion.button
                         initial={{ opacity: 0, y: 25 }}
@@ -116,14 +116,14 @@ const Footer = () => {
                         transition={{ type: 'spring', duration: 0.5, delay: 0.5 }}
                         viewport={{ once: true }}
                         className="rounded-lg shadow-lg shadow-textColor/50 border border-textColor p-2">
-                        <img src={tiktok} alt="Tiktok" className="w-8 h-8" />
+                        <img src={tiktok} alt="Tiktok" className="w-6 h-6" />
                     </motion.button>
                 </div>
             </article>
 
-            <hr className="border-textColor w-full" />
+            <hr className="border-textColor w-full lg:col-span-full" />
 
-            <p className="italic font-impact">
+            <p className="italic font-impact lg:col-span-full lg:text-center">
                 &copy; {new Date().getFullYear()} Crouton Jones. All rights reserved.
             </p>
         </section>
